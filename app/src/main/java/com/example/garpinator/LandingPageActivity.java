14e3173a870470f519698c9bd168cf77d0084e4a
AmielFabreguettes <amielfabreguettes@gmail.com>
2024-04-19 01:53:49 -0700

    merged UserDatabase in Main

--- app/src/main/java/com/example/garpinator/LandingPageActivity.java
+++ app/src/main/java/com/example/garpinator/LandingPageActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
-
-
+public class LandingPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_landing_page);
     }
 }
