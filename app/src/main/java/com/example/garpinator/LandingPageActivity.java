85aaf5ad1e8e0823fe2fa3563453b72c4d3cd13c
AmielFabreguettes <amielfabreguettes@gmail.com>
2024-04-19 15:10:44 -0700

    added login added admin button

--- app/src/main/java/com/example/garpinator/LandingPageActivity.java
+++ app/src/main/java/com/example/garpinator/LandingPageActivity.java
@@ -2,13 +2,26 @@ package com.example.garpinator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingPageActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        this.prefs = getSharedPreferences("LoginActivity",Context.MODE_PRIVATE);
+        if(prefs.getBoolean("isAdmin",false)){
+            Button admin_button = findViewById(R.id.admin_button);
+            admin_button.setVisibility(View.VISIBLE);
+        }
+
     }
 }
--- app/src/main/java/com/example/garpinator/LoginActivity.java
+++ app/src/main/java/com/example/garpinator/LoginActivity.java
@@ -2,9 +2,23 @@ package com.example.garpinator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private GarpinatorRepo db;
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor prefsEdit;
 
 
 
@@ -12,6 +26,36 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.db = new GarpinatorRepo(getApplication());
+        this.prefs = getSharedPreferences("LoginActivity",Context.MODE_PRIVATE);
+        this.prefsEdit = prefs.edit();
+
+        Button sumbitButton = findViewById(R.id.login);
+
+        sumbitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText username_input = findViewById(R.id.logininput);
+                String entered_username = username_input.getText().toString();
+
+                EditText password_input = findViewById(R.id.password);
+                String password = password_input.getText().toString();
+
+                User user = db.getUserByName(entered_username);
+                if(user == null || !Objects.equals(user.getPassword(), password)){
+                    Toast.makeText(LoginActivity.this,
+                            "Username or password incorrect.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    prefsEdit.putString("curUser",user.getUsername());
+                    prefsEdit.putBoolean("isAdmin",user.isAdmin());
+                    prefsEdit.apply();
+                    Intent intent = new Intent(LoginActivity.this, LandingPageActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
 
     }
 }
