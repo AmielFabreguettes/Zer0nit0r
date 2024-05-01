51130b23750815d9505297e4f5ef02f6cce3a9f2
NoobMan291 <noobman.04ef@gmail.com>
2024-05-01 16:16:30 -0700

    Huge Succes Game is kinda working
    Breakthrough, Questions shows up now. Just need to implement all the others questions now.

--- app/src/main/java/com/example/garpinator/GameActivity.java
+++ app/src/main/java/com/example/garpinator/GameActivity.java
@@ -37,10 +37,18 @@ public class GameActivity extends AppCompatActivity {
     private boolean vsSanji;
     private boolean haki;
     private boolean answered;
+    Random rand;
 
     List<String> crews;
     List<String> races;
     List<String> weapons;
+    List<Pirate> pirates;
+
+    TextView question;
+    Button yes;
+    Button no;
+    Button idk;
+    int question_time;
 
     private int min_age = 0;
     private int max_age = Integer.MAX_VALUE;
@@ -58,22 +66,14 @@ public class GameActivity extends AppCompatActivity {
         // Ask if you know the rules
         // Then do stuff
 
-        TextView question = findViewById(R.id.question);
-        Button yes = findViewById(R.id.yes);
-        Button no = findViewById(R.id.no);
-        Button idk = findViewById(R.id.idk);
+        question = findViewById(R.id.question);
+        yes = findViewById(R.id.yes);
+        no = findViewById(R.id.no);
+        idk = findViewById(R.id.idk);
 
-        Random rand = new Random();
+        rand = new Random();
 
-        game(question, yes, no, idk, rand);
-
-    }
-
-    private void game(TextView question, Button yes, Button no, Button idk, Random rand){
-
-        boolean found = false;
-
-        List<Pirate> pirates = GarpinatorDatabase.retrieveAllPirates();
+        pirates = GarpinatorDatabase.retrieveAllPirates();
 
         crews = new ArrayList<>();
         for (Pirate pirate : pirates){
@@ -93,179 +93,112 @@ public class GameActivity extends AppCompatActivity {
                 weapons.add(pirate.getWeapon());
         }
 
-        while (!found){
-            int question_time = 0;//rand.nextInt(18);
-            if (question_time == 0 && !crew){
-                int chosen_crew = rand.nextInt(crews.size());
-                question.setText("Is your character part of the " + crews.get(chosen_crew));
-                crew(yes, no, idk, pirates, chosen_crew);
-            }
-            ////////////////////////////////////////////////////////////////////////////////////////
-            else if (question_time == 1 && (!bounty || !known_bounty)){
-                if (!known_bounty){
-                    question.setText("Does your character have a known bounty ?");
-                    known_bounty = !known_bounty;
-                    known_bounty(yes, no, idk, pirates);
-                    answered = !answered;
-                    if (!known_bounty_answer){
-                        bounty = !bounty;
-                    }
+        game();
 
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        idk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void game(){
+
+        question_time = 0;//rand.nextInt(18);
+        if (question_time == 0 && !crew){
+            int chosen_crew = rand.nextInt(crews.size());
+            question.setText("Is your character part of the " + crews.get(chosen_crew));
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////
+        else if (question_time == 1 && (!bounty || !known_bounty)){
+            if (!known_bounty){
+                question.setText("Does your character have a known bounty ?");
+                known_bounty = !known_bounty;
+                answered = !answered;
+                if (!known_bounty_answer){
+                    bounty = !bounty;
                 }
-                else if(!bounty){
-                    // calculate average;
-                }
-            }
-            ////////////////////////////////////////////////////////////////////////////////////////
-            else if (question_time == 2 && !nickname){
-                question.setText("Does your character have a nickname ?");
-                nickname(yes, no, idk, pirates);
-                nickname = !nickname;
-            }
-            else if (question_time == 3 && !status){
-                question.setText("Is your character alive ?");
-                status = !status;
-            }
-            else if (question_time == 4 && !age){
 
             }
-            else if (question_time == 5 && !birthday){
-
-            }
-            else if (question_time == 6 && !height){
-
-            }
-            else if (question_time == 7 && !devil_fruit){
-                question.setText("Does your character have a devil fruit power ?");
-                devil_fruit = !devil_fruit;
-
-            }
-            else if (question_time == 8 && !yonko_crew){
-                question.setText("Is your character part of a Yonko crew ?");
-                yonko_crew = !yonko_crew;
-            }
-            else if (question_time == 9 && !yonko){
-                question.setText("Is your character a Yonko ?");
-                yonko = !yonko;
-            }
-            else if (question_time == 10 && !scar){
-                question.setText("Does your character have an apparant scar ?");
-                scar = !scar;
-            }
-            else if (question_time == 11 && !weapon){
-
-            }
-            else if (question_time == 12 && !race){
-
-            }
-            else if (question_time == 13 && !captain){
-                question.setText("Is your character captain of a crew ?");
-                captain = !captain;
-            }
-            else if (question_time == 14 && !vsLuffy){
-                question.setText("Did your character fight angaints Luffy ?");
-                vsLuffy = !vsLuffy;
-            }
-            else if (question_time == 15 && !vsZoro){
-                question.setText("Did your character fight angaints Zoro ?");
-                vsZoro = !vsZoro;
-            }
-            else if (question_time == 16 && !vsSanji){
-                question.setText("Did your character fight angaints Sanji ?");
-                vsSanji = !vsSanji;
-            }
-            else if (question_time == 17 && !haki){
-                question.setText("Does your character know any type of haki ?");
-                haki = !haki;
+            else if(!bounty){
+                // calculate average;
             }
         }
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////
-    private void crew(Button yes, Button no, Button idk, List<Pirate> pirates, int chosen_crew){
-        while (!answered){
-            yes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pirates.removeIf(pirate -> !pirate.getCrew().equals(crews.get(chosen_crew)));
-                    crew = !crew;
-                    answered = !answered;
-                }
-            });
+        ////////////////////////////////////////////////////////////////////////////////////////
+        else if (question_time == 2 && !nickname){
+            question.setText("Does your character have a nickname ?");
+            nickname = !nickname;
+        }
+        else if (question_time == 3 && !status){
+            question.setText("Is your character alive ?");
+            status = !status;
+        }
+        else if (question_time == 4 && !age){
 
-            no.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pirates.removeIf(pirate -> !pirate.getCrew().equals(crews.get(chosen_crew)));
-                    crews.remove(chosen_crew);
-                    answered = !answered;
-                }
-            });
+        }
+        else if (question_time == 5 && !birthday){
 
-            idk.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    crew = !crew;
-                    answered = !answered;
-                }
-            });
+        }
+        else if (question_time == 6 && !height){
+
+        }
+        else if (question_time == 7 && !devil_fruit){
+            question.setText("Does your character have a devil fruit power ?");
+            devil_fruit = !devil_fruit;
+
+        }
+        else if (question_time == 8 && !yonko_crew){
+            question.setText("Is your character part of a Yonko crew ?");
+            yonko_crew = !yonko_crew;
+        }
+        else if (question_time == 9 && !yonko){
+            question.setText("Is your character a Yonko ?");
+            yonko = !yonko;
+        }
+        else if (question_time == 10 && !scar){
+            question.setText("Does your character have an apparant scar ?");
+            scar = !scar;
+        }
+        else if (question_time == 11 && !weapon){
+
+        }
+        else if (question_time == 12 && !race){
+
+        }
+        else if (question_time == 13 && !captain){
+            question.setText("Is your character captain of a crew ?");
+            captain = !captain;
+        }
+        else if (question_time == 14 && !vsLuffy){
+            question.setText("Did your character fight angaints Luffy ?");
+            vsLuffy = !vsLuffy;
+        }
+        else if (question_time == 15 && !vsZoro){
+            question.setText("Did your character fight angaints Zoro ?");
+            vsZoro = !vsZoro;
+        }
+        else if (question_time == 16 && !vsSanji){
+            question.setText("Did your character fight angaints Sanji ?");
+            vsSanji = !vsSanji;
+        }
+        else if (question_time == 17 && !haki){
+            question.setText("Does your character know any type of haki ?");
+            haki = !haki;
         }
     }
-    ////////////////////////////////////////////////////////////////////////////////////////
-    private void known_bounty(Button yes, Button no, Button idk, List<Pirate> pirates){
-        while (!answered){
-            yes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pirates.removeIf(pirate -> pirate.getBounty().equals("-1"));
-                    known_bounty_answer = true;
-                    answered = !answered;
-                }
-            });
-
-            no.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pirates.removeIf(pirate -> !pirate.getBounty().equals("-1"));
-                    known_bounty_answer = false;
-                    answered = !answered;
-                }
-            });
-
-            idk.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    known_bounty_answer = false;
-                    answered = !answered;
-                }
-            });
-        }
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////
-    private void nickname(Button yes, Button no, Button idk, List<Pirate> pirates){
-        while (!answered){
-            yes.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pirates.removeIf(pirate -> !pirate.isNickname());
-                    answered = !answered;
-                }
-            });
-
-            no.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pirates.removeIf(Pirate::isNickname);
-                    answered = !answered;
-                }
-            });
-
-            idk.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    answered = !answered;
-                }
-            });
-        }
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////
 }
